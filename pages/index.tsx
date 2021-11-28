d3d8dca3eb6bf0bb7722cc85ad99eb93645f4d80
wj2kim <wj2kim@gmail.com>
2021-11-28 22:53:31 +0900

    feat: getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,24 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import utilStyles from '../styles/utils.module.css'
 import Layout, { siteTitle } from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
 
-const Home: NextPage = () => {
+export const getStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+}
+
+type AllPostsData = {
+  id: string
+  date: Date
+  title: string
+}
+
+const Home = ({ allPostsData }: { allPostsData: AllPostsData[] }) => {
   return (
     <Layout home>
       <Head>
@@ -15,6 +31,20 @@ const Home: NextPage = () => {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
