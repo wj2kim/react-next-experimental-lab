c654640c548be2ece2d7bbe64e09f657f556256a
wj2kim <wj2kim@gmail.com>
2021-11-28 23:45:35 +0900

    feat: dynamic router using getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import utilStyles from '../styles/utils.module.css'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export const getStaticProps = async () => {
   const allPostsData = getSortedPostsData()
@@ -37,11 +39,13 @@ const Home = ({ allPostsData }: { allPostsData: AllPostsData[] }) => {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
